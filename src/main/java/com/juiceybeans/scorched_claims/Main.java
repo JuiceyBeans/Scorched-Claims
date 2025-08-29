7b29bb8e1be56cca89f184070f01852a107d7f61
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-08-30 01:02:30 +0400

    Mod setup

---
+++ src/main/java/com/juiceybeans/scorched_claims/Main.java
@@ -0,0 +1,31 @@
+package com.juiceybeans.scorched_claims;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Main.MOD_ID)
+public class Main {
+
+    public static final String MOD_ID = "scorched_claims";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+}
