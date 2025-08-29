81eaeab2ea2ca96b1841f67a248b7dc0d4e33a9f
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-08-30 02:23:43 +0400

    Simplify claim result logic

--- src/main/java/com/juiceybeans/scorched_claims/block/ClaimBlock.java
+++ src/main/java/com/juiceybeans/scorched_claims/block/ClaimBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import xaero.pac.common.claims.result.api.ClaimResult;
 import xaero.pac.common.server.api.OpenPACServerAPI;
 
 public class ClaimBlock extends Block {
@@ -24,6 +23,8 @@ public class ClaimBlock extends Block {
     public InteractionResult use(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand hand,
                                  BlockHitResult hitResult) {
         var itemStack = player.getMainHandItem();
+        InteractionResult result = InteractionResult.FAIL;
+
         if (itemStack.is(Items.GOLD_INGOT)) {
             var opacAPI = Minecraft.getInstance().isLocalServer()
                     ? OpenPACServerAPI.get(Minecraft.getInstance().getSingleplayerServer())
@@ -36,49 +37,21 @@ public class ClaimBlock extends Block {
                 player.displayClientMessage(
                         Component.translatable("You are not currently in a party!").withStyle(ChatFormatting.RED),
                         true);
-                return InteractionResult.FAIL;
+                return result;
             }
-
 
             var claim = opacAPI.getServerClaimsManager().tryToClaim(level.dimension().location(), party.getOwner().getUUID(),
                     0, pos.getX(), pos.getZ(), pos.getX(), pos.getZ(), false);
             //todo figure out why the claim is so far away from clicked block
             //todo figure out what the hell subconfig index means
 
-            if (claim.getResultType() == ClaimResult.Type.SUCCESSFUL_CLAIM) {
-                player.displayClientMessage(
-                        ClaimResult.Type.SUCCESSFUL_CLAIM.message,
-                        false);
+            if (claim.getResultType().success) {
                 itemStack.shrink(1);
-                return InteractionResult.SUCCESS;
-            } else {
-                if (claim.getResultType() == ClaimResult.Type.ALREADY_CLAIMED) {
-                    player.displayClientMessage(
-                            ClaimResult.Type.ALREADY_CLAIMED.message,
-                            true);
-                }  else if (claim.getResultType() == ClaimResult.Type.UNCLAIMABLE_DIMENSION) {
-                    player.displayClientMessage(
-                            ClaimResult.Type.UNCLAIMABLE_DIMENSION.message,
-                            true);
-                }  else if (claim.getResultType() == ClaimResult.Type.CLAIM_LIMIT_REACHED) {
-                    player.displayClientMessage(
-                            ClaimResult.Type.CLAIM_LIMIT_REACHED.message,
-                            true);
-                }  else if (claim.getResultType() == ClaimResult.Type.CLAIMS_ARE_DISABLED) {
-                    player.displayClientMessage(
-                            ClaimResult.Type.CLAIMS_ARE_DISABLED.message,
-                            true);
-                }  else if (claim.getResultType() == ClaimResult.Type.NO_SERVER_PERMISSION) {
-                    player.displayClientMessage(
-                            ClaimResult.Type.NO_SERVER_PERMISSION.message,
-                            true);
-                }
-
-                return InteractionResult.FAIL;
+                result = InteractionResult.SUCCESS;
             }
-
+            player.displayClientMessage(claim.getResultType().message, false);
         }
 
-        return InteractionResult.PASS;
+        return result;
     }
 }
