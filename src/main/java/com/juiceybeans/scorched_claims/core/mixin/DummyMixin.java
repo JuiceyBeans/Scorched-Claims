012e168d846d04d368cc570488cbcce50efce1c1
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2025-09-08 09:14:00 +0100

    Migrate to ModDevGradle

---
+++ src/main/java/com/juiceybeans/scorched_claims/core/mixin/DummyMixin.java
@@ -0,0 +1,10 @@
+package com.juiceybeans.scorched_claims.core.mixin;
+
+import net.minecraft.world.level.levelgen.WorldgenRandom;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+// Dummy mixin so that MDG doesn't throw a fit
+// Todo remove if we ever have another mixin
+@Mixin(value = WorldgenRandom.class, remap = false)
+public class DummyMixin {}
